32a09ff0e15435154979e56f663abeb7b96bed57
perumal59 <79631950+perumal59@users.noreply.github.com>
2023-05-22 15:40:12 +0530

    Add files via upload

---
+++ Resume/main.js
@@ -0,0 +1,18 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+let header = document.querySelector('header');
+header.classList.toggle('sticky',window.scrollY > 100);
+
+ScrollReveal({
+    reset:true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+ScrollReveal().reveal('.home-content, .heading ,education-container',{orgin:'top'});
